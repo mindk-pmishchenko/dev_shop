c60b70184904d2c43a6d78666c44f43d88ab04f4
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-03-27 17:26:43 +0200

    fix(helpers): fix checkPath helper logic

--- frontend/src/utils/helper/checkPath.js
+++ frontend/src/utils/helper/checkPath.js
@@ -5,12 +5,24 @@ const checkPath = (path, categories) => {
 
   const categorySlugList = pathParts.filter(path => path !== 'category' && path !== '')
 
-  for (const slug of categorySlugList) {
-    const check = returnFound(categories, { slug })
-    if (check === undefined) {
+  for (let i = 0; i < categorySlugList.length; i++) {
+    const nextSlug = categorySlugList[i]
+    const nextCategory = returnFound(categories, { slug: nextSlug })
+
+    const prevSlug = categorySlugList[i - 1]
+    const prevCategory = returnFound(categories, { slug: prevSlug })
+
+    if (!nextCategory) {
       return false
     }
+
+    if (prevCategory) {
+      if (nextCategory.parentId !== prevCategory.id) {
+        return false
+      }
+    }
   }
+
   return true
 }
 
