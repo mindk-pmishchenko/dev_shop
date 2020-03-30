016f0e395f9f3f899b14508d295e2f66adf85f95
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-03-30 22:41:42 +0300

    fix(helpers): fix getCategory helper logic

--- frontend/src/utils/helper/getCategory.js
+++ frontend/src/utils/helper/getCategory.js
@@ -1,7 +1,8 @@
 import { returnFound } from 'find-and'
 
 const getCategory = (path, categories) => {
-  const pathParts = path.split('/')
+  const pathParts = path.split('/').filter(path => path !== '')
+
   const slug = pathParts[pathParts.length - 1]
   return returnFound(categories, { slug })
 }
