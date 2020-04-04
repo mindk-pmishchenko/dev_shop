d9eeb51f2eb220dc159ae30133a15a3f0e94e44e
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-04-04 20:13:37 +0300

    feat(Cart): implementing cart

---
+++ frontend/src/components/Cart/CartDetails/CartDetails.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import TableContainer from '@material-ui/core/TableContainer'
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
+import AddIcon from '@material-ui/icons/Add'
+import RemoveIcon from '@material-ui/icons/Remove'
+import PropTypes from 'prop-types'
+
+import product from '../../../types/product'
+
+const CartDetails = ({ products, handleDeleteProduct, setProductCount }) => {
+  const total = products.reduce((acc, product) => acc + product.price * product.quantity, 0)
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Название: </TableCell>
+            <TableCell align="right">Цена: </TableCell>
+            <TableCell align="right">Количество: </TableCell>
+            <TableCell align="right">Сумма: </TableCell>
+            <TableCell align="right">Удалить: </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {products.map(({ id, name, price, quantity }) => {
+            return (
+              <TableRow key={id}>
+                <TableCell>{name}</TableCell>
+                <TableCell align="right">{price}</TableCell>
+                <TableCell align="right">
+                  <IconButton onClick={setProductCount(id)(-1)}>
+                    <RemoveIcon />
+                  </IconButton>
+                  {`  ${quantity}  `}
+                  <IconButton onClick={setProductCount(id)(+1)}>
+                    <AddIcon />
+                  </IconButton>
+                </TableCell>
+                <TableCell align="right">{price * quantity}</TableCell>
+                <TableCell align="right">
+                  <IconButton onClick={handleDeleteProduct(id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            )
+          })}
+          <TableRow>
+            <TableCell rowSpan={1} />
+            <TableCell colSpan={2} align="center">
+              Итого:
+            </TableCell>
+            <TableCell align="right">{total}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+CartDetails.propTypes = {
+  products: PropTypes.arrayOf(product).isRequired,
+  handleDeleteProduct: PropTypes.func,
+  setProductCount: PropTypes.func
+}
+
+export default CartDetails
--- frontend/src/containers/Layout/Layout.jsx
+++ frontend/src/containers/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import Grid from '@material-ui/core/Grid'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
@@ -6,29 +6,61 @@ import useDataApi from '../../utils/hooks/useDataApi'
 import Menu from '../Menu/Menu'
 import Spinner from '../../components/Spinner/Spinner'
 import Category from '../Category/Category'
+import Cart from '../../components/Cart/Cart'
 import Error from '../../components/Error/Error'
 import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary'
+import CartContext from '../../context/cartContext'
 import useStyles from './styles'
 
 const Layout = () => {
-  const classes = useStyles()
-
   const { rawData, isLoading, isError } = useDataApi({ url: '/categories?filter={"limit": 100}', method: 'GET' })
   const categories = rawData && !isError ? rawData.results : []
+
+  const [openCart, setOpenCart] = useState(false)
+  const handleCloseCart = () => setOpenCart(false)
+
+  const { cart, setCart } = useContext(CartContext)
+
+  const handleDeleteProduct = (id) => () => {
+    const filteredProducts = cart.products.filter((product) => product.id !== id)
+    if (filteredProducts.length === 0) {
+      const newCart = { ...cart }
+      delete newCart.products
+      setCart(newCart)
+    } else {
+      setCart({ ...cart, products: filteredProducts })
+    }
+  }
+
+  const setProductCount = (id) => (count) => () => {
+    setCart({
+      ...cart,
+      products: cart.products.map((product) => {
+        if (product.id === id && !(product.quantity <= 1 && count < 0)) {
+          product.quantity += count
+        }
+        return product
+      })
+    })
+  }
+
+  const classes = useStyles()
 
   return (
     <ErrorBoundary>
       <BrowserRouter>
         <Grid container className={classes.app}>
-          <Grid container direction="row">
-            <Grid className={classes.menu}>{isLoading ? <Spinner /> : <Menu categories={categories} />}</Grid>
-            <Grid className={classes.mainSection}>
+          <Grid container direction="row" spacing={3}>
+            <Grid className={classes.menu1} item xs={12} sm={4} md={3}>
+              {isLoading ? <Spinner /> : <Menu categories={categories} />}
+            </Grid>
+            <Grid className={classes.mainSection1} item xs={12} sm={8} md={9}>
               <Switch>
                 <Route exact path="/">
                   Main page
                 </Route>
                 <Route path="/category">
-                  <Category categories={categories} />
+                  <Category categories={categories} setOpenCart={setOpenCart} />
                 </Route>
                 <Route>
                   <Error />
@@ -38,6 +70,13 @@ const Layout = () => {
           </Grid>
         </Grid>
       </BrowserRouter>
+      <Cart
+        open={openCart}
+        onClose={handleCloseCart}
+        cart={cart}
+        handleDeleteProduct={handleDeleteProduct}
+        setProductCount={setProductCount}
+      />
     </ErrorBoundary>
   )
 }
