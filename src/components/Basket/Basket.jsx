e08a9ffe320e660fb3517f1d189f573e1b28070f
Maxim Rudenko <m.rudenko@cis.sumdu.edu.ua>
2020-04-08 20:30:59 +0300

    feat(basket) add basket and basket product components

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -8,9 +8,8 @@ import Typography from "@material-ui/core/Typography";
 import BasketProduct from "./BasketProduct/BasketProduct";
 
 function Basket({open, onClose, basket, handleDeleteProduct, setProductCount}) {
-    const products = Object.entries(basket);
-    const baskIsEmpty = !!products.length;
-    const total = 100;
+    const baskIsEmpty = !basket.length;
+    const total = basket.reduce((total, product) => total + (product.count * product.price), 0);
 
     return (
         <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
@@ -30,24 +29,34 @@ function Basket({open, onClose, basket, handleDeleteProduct, setProductCount}) {
                     </Grid>
                 )}
                 {!baskIsEmpty && (
-                    <Grid container>
-                        {products.map(product => <BasketProduct product={product} />)}
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}>
+                            <Typography variant="h6" >Title</Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6">Price</Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6">Count</Typography>
+                        </Grid>
+                        <Grid item xs={2}/>
+                        {basket.map(product => <BasketProduct product={product} handleDeleteProduct={handleDeleteProduct} setProductCount={setProductCount}/>)}
                     </Grid>
                 )}
             </DialogContent>
             <Grid container>
-                <Grid item xs={3}>
+                <Grid item xs={4}>
+                    Back
+                </Grid>
+                <Grid item xs={4}>
+                    Buy
+                </Grid>
+                <Grid item xs={4}>
                     <Grid container justify="center">
                         <Typography variant="h5">
                             {`Total: $${total}`}
                         </Typography>
                     </Grid>
-                </Grid>
-                <Grid item xs={3}>
-                    Back
-                </Grid>
-                <Grid item xs={3}>
-                    Buy
                 </Grid>
             </Grid>
         </Dialog>
--- src/components/Basket/BasketProduct/BasketProduct.jsx
+++ src/components/Basket/BasketProduct/BasketProduct.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import {Delete} from "@material-ui/icons";
 
-function BasketProduct({product}) {
-    return 'Basket item';
+function BasketProduct({product, setProductCount, handleDeleteProduct}) {
+
+    const [count, setCount] = useState(product.count);
+
+    const onChangeCount = (event) => {
+        setCount(event.target.value);
+        setProductCount(product.id, event.target.value);
+    };
+
+    return (<>
+        <Grid item xs={4}>
+            <Typography>{product.title}</Typography>
+        </Grid>
+        <Grid item xs={3}>
+            <Typography>{product.price}</Typography>
+        </Grid>
+        <Grid item xs={3}>
+            <TextField id="outlined-basic"
+                       label="Outlined"
+                       type="number"
+                       variant="outlined"
+                       size="small"
+                       inputProps={{ min: "1" }}
+                       value={product.count}
+                       onChange={onChangeCount}
+            />
+        </Grid>
+        <Grid item xs={2}>
+            <IconButton color="inherit" onClick={handleDeleteProduct(product.id)}>
+                <Delete />
+            </IconButton>
+        </Grid>
+    </>);
 }
 
 export default BasketProduct;
