96ea7f8dc77cbdeecde21cf3d9bf9cf79f29e3fa
Maxim Rudenko <m.rudenko@cis.sumdu.edu.ua>
2020-04-08 20:13:09 +0300

    feat(basket) add opening basket on click on button in toolbar

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -8,18 +8,22 @@ import Badge from "@material-ui/core/Badge";
 
 
 import {useStyles} from "./styles";
+import BasketContext from "../../context/basketContext";
 
-function TopBar() {
+
+function TopBar({setOpenBasket}) {
     const classes = useStyles();
+
+    const {basket} = useContext(BasketContext);
 
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="h1" className={classes.title} variant="h6">
+                <Typography className={classes.title} component="h1" variant="h6">
                     Super Shop
                 </Typography>
-                <IconButton aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={1} color="secondary">
+                <IconButton color="inherit" onClick={() => setOpenBasket(true)}>
+                    <Badge badgeContent={basket.length} color="secondary">
                         <ShoppingCart />
                     </Badge>
                 </IconButton>
