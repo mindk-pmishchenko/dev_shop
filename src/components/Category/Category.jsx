cf6eb65c2d53cc8db63fab6ec4d523160a1676f8
Maxim Rudenko <m.rudenko@cis.sumdu.edu.ua>
2020-04-08 10:24:50 +0300

    feat(category) add request for products and add styles

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import {withRouter} from "react-router";
 import PropTypes from 'prop-types';
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Typography from "@material-ui/core/Typography";
 
 import category from "../../types/category";
+import useDataApi from "../../utils/hooks/useDataApi";
+import Product from "../Product/Product";
+import {useStyles} from "./styles";
+
 
 function Category({categories, match: {params}}) {
+    const classes = useStyles();
     const {alias} = params;
     const category = categories.find(({alias: categoryAlias}) => categoryAlias === alias);
+
+    const { rawData, isLoading } = useDataApi({ url: '/products' });
 
     if (!category) {
         return '404';
     }
-
-    return `#${category.id} - ${category.title}`;
+    return <>
+        <Typography className={classes.category} variant="h2">
+            {`${category.id} - ${category.title}`}
+        </Typography>
+        {isLoading && <CircularProgress />}
+        {rawData && <Product products={rawData} cat_id={category.id}/>}
+    </>;
 }
 
 Category.propTypes = {
---
+++ src/components/Category/styles.js
@@ -0,0 +1,8 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+export const useStyles = makeStyles({
+    category: {
+        fontSize: '20px',
+        marginBottom: '20px'
+    }
+});
