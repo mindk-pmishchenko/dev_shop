316923d3fea5f5ccc08f59056f24551f283f61a4
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-04-17 13:39:44 +0300

    feat(containers): add routes to Layout

--- frontend/src/containers/Layout/styles.js
+++ frontend/src/containers/Layout/styles.js
@@ -1,17 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(() => ({
-  app: {
-    height: '100vh',
-    width: '100%'
-  },
-  menu: {
-    height: '100%',
-    width: '300px'
-  },
-  mainSection: {
-    height: '100%',
-    width: 'calc(100% - 300px)'
+  appBarContainer: {
+    marginBottom: 15
   }
 }))
 
